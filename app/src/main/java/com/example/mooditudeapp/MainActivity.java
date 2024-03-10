6d305ad419ca2d16c34c95cb07f560e322667301
William <williamma@hotmail.ca>
2024-03-10 13:59:32 -0400

    Added Firebase Firestore to our app.

--- app/src/main/java/com/example/mooditudeapp/MainActivity.java
+++ app/src/main/java/com/example/mooditudeapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
